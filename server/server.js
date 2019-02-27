a8ce6a1ed865fd5120f28fff545268f373e4ac5d
gerry <gerry@gmail.com>
2019-02-28 00:55:33 +0800

    Init commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
